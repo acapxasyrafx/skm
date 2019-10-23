c64ae828233017b331b178259bb3bf8412405a9d
Amirul Aiman <aiman_amirul@ymail.com>
2019-10-23 17:46:50 +0800

    Verify admin or user on homepage.aspx

--- QuartersManagement/Admin.Homepage.aspx.vb
+++ QuartersManagement/Admin.Homepage.aspx.vb
@@ -2,7 +2,12 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim user_type = CType(Session("user_type"), String)
+        If user_type.Equals("Admin") Then
 
+        Else
+            Response.Redirect("User.Homepage.aspx")
+        End If
     End Sub
 
 End Class
--- QuartersManagement/User.Homepage.aspx.vb
+++ QuartersManagement/User.Homepage.aspx.vb
@@ -2,7 +2,12 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim user_type = CType(Session("user_type"), String)
+        If user_type.Equals("User") Then
 
+        Else
+            Response.Redirect("Admin.Homepage.aspx")
+        End If
     End Sub
 
 End Class
