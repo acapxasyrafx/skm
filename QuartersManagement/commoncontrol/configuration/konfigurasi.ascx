9eb700bad49165fafe278c1924fa5a60f6944305
Amirul Aiman <aiman_amirul@ymail.com>
2019-10-30 10:04:15 +0800

    Tambah link konfigurasi
    - Jawatan
    - Point
    - Jantina

--- QuartersManagement/commoncontrol/configuration/konfigurasi.ascx
+++ QuartersManagement/commoncontrol/configuration/konfigurasi.ascx
@@ -9,6 +9,16 @@
     <tr>
         <td><img src ="icons/bullet_red.png" /><a href ="Konfigurasi.Khusus.aspx?q=NEGERI&p=Konfigurasi Negeri" class ="link">Konfigurasi Negeri</a></td>
     </tr>
+    <tr>
 
-
+    </tr>
+    <tr>
+        <td><img src ="icons/bullet_red.png" /><a href ="Konfigurasi.Khusus.aspx?q=JANTINA&p=Konfigurasi Jantina" class ="link">Konfigurasi Jantina</a></td>
+    </tr>
+    <tr>
+        <td><img src ="icons/bullet_red.png" /><a href ="Konfigurasi.Khusus.aspx?q=JAWATAN&p=Konfigurasi Jawatan" class ="link">Konfigurasi Jawatan</a></td>
+    </tr>
+    <tr>
+        <td><img src ="icons/bullet_red.png" /><a href ="Konfigurasi.Khusus.aspx?q=POINT&p=Konfigurasi Markah" class ="link">Konfigurasi Markah</a></td>
+    </tr>
 </table>
--- QuartersManagement/commoncontrol/menu/index.ascx.vb
+++ QuartersManagement/commoncontrol/menu/index.ascx.vb
@@ -1,11 +1,16 @@
 ﻿Public Class index
     Inherits System.Web.UI.UserControl
-
+    Dim user_type As String = ""
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Dim user_type = CType(Session("user_type"), String)
-        If user_type.Equals("Admin") Then
-        ElseIf user_type.Equals("User") Then
-            PnlPengurusan.Visible = False
+        user_type = CType(Session("user_type"), String)
+        If user_type.Equals("") Then
+            Response.Redirect("Default.aspx")
+        Else
+            If user_type.Equals("Admin") Then
+            ElseIf user_type.Equals("User") Then
+                PnlPengurusan.Visible = False
+                PnlKonfigurasi.Visible = False
+            End If
         End If
     End Sub
 
